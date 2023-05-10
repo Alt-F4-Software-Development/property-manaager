9286275fb1c4d1cf1c95f13245676c3b3d83d9df
WasteofSpaceYT <WasteofSpaceYT@outlook.com>
2023-05-10 16:12:33 -0400

    changes

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,9 +1,13 @@
-<script>
+<script lang="ts">
 	import '../app.postcss';
+	export let data
 </script>
 
 <div class="bg-slate-50 rounded-b h-50 py-2">
 	<a href="/" class="ml-4"><button class="bg-black border-white rounded px-2 py-1 shadow-sm text-white">Home</button></a>
 	<a href="/new" class="ml-4"><button class="bg-black border-white rounded px-2 py-1 shadow-sm text-white">New Property</button></a>
+	{#if data.name != undefined}
+		<a href="/{data.name?.toLowerCase()}" class="ml-4"><button class="bg-black border-white rounded px-2 py-1 shadow-sm text-white">View Property</button></a>
+	{/if}
 </div>
 <slot />
---
+++ website/src/routes/[property]/financing/+page.ts
@@ -0,0 +1,5 @@
+export const load = ({ params }) => {
+    console.log(params);
+    const name = params.property;
+    return { name };
+}
